852d5774de2f039cc7737f1ff30efa14c03d3462
Leonard <84669366+Leonator26@users.noreply.github.com>
2023-07-17 08:54:09 +0200

    Update SetBefehl.java

--- SetBefehl.java
+++ SetBefehl.java
@@ -4,6 +4,11 @@ public class SetBefehl {
       System.err.println("Es werden drei Argumente erwartet: (x) (y) ('x'/'o'); Beispiel: 'set 1 2 o'");
       return;
     }
+    if (args[3].equals("x") || args[3].equals("o")) {
+
+    } else {
+      System.out.println("Eingabe Falsch");
+      }
     CLIMain.BRETT.setFigur(Integer.parseInt(args[1]), Integer.parseInt(args[2]), args[3]);
   }
 }
